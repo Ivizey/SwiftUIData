b08926775f646d83821279ee2a482ac9bab6062d
Pavel Bondar <ivizey@gmail.com>
2019-11-16 17:17:16 +0200

    Added Video model

---
+++ SwiftUIDataTransfer/VideoModel.swift
@@ -0,0 +1,21 @@
+//
+//  VideoModel.swift
+//  SwiftUIDataTransfer
+//
+//  Created by Pavel Bondar on 16.11.2019.
+//  Copyright © 2019 Pavel Bondar. All rights reserved.
+//
+
+import SwiftUI
+
+struct VideoModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VideoModel_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoModel()
+    }
+}
