65d10b4a080e2b1052cfd5346a7a6e44c898f1ad
Pavel Bondar <ivizey@gmail.com>
2019-11-16 15:29:54 +0200

    Initial Commit

---
+++ SwiftUIDataTransfer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDataTransfer
+//
+//  Created by Pavel Bondar on 16.11.2019.
+//  Copyright © 2019 Pavel Bondar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
